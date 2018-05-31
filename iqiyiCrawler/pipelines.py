3db5d7e3a3af88c0279a25fbd9cad3830fea02b5
chenxy0761 <chenxy0761@163.com>
2018-05-31 09:47:28 +0800

    PIPELINES

--- iqiyiCrawler/pipelines.py
+++ iqiyiCrawler/pipelines.py
@@ -5,14 +5,34 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from iqiyiCrawler.con_sql import Dba
-from iqiyiCrawler.items import IqiyicrawlerItem
+from iqiyiCrawler.items import IqiyicrawlerItem, IqiyiTVItem, IqiyiCartoonItem, IqiyiChildItem, IqiyiShowItem
 
 
 class IqiyicrawlerPipeline(object):
     def __init__(self):
         self.ora = Dba()
         self.Movie_Sql = """
-                           INSERT INTO `douban`.`iqiyi_movie`(`albumId`, `name`, `directors`, `Actors`, `period`, `description`, `pay`, `score`, `starTotal`, `tags`, `type`, `spec`, `positive`, `sex`, `ages`, `duration`) 
+                           INSERT INTO douban.iqiyi_movie(albumId, name, directors, Actors, period, description, pay, score, starTotal, tags, type, spec, positive, sex, ages, duration) 
+                           VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
+
+                        """
+        self.Tv_Sql = """
+                           INSERT INTO douban.iqiyi_tv(url, name, directors, Actors, period, description, pay, score, starTotal, tags, type, spec, positive, sex, ages, subtitle, numTotal) 
+                           VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
+
+                        """
+        self.cartoon_Sql = """
+                           INSERT INTO douban.iqiyi_cartoon(url, name, Actors, period, description, pay, score, total, type, region, sex, ages, subtitle, number, version, inLanguage) 
+                           VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
+
+                        """
+        self.child_Sql = """
+                           INSERT INTO douban.iqiyi_child(url, name, Actors, period, description, pay, score, total, type, region, sex, ages, subtitle, number, version, inLanguage) 
+                           VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
+
+                        """
+        self.show_Sql = """
+                           INSERT INTO douban.iqiyi_show(url, name, Actors, period, description, pay, score, total, type, region, sex, ages, subtitle, number, version, inLanguage) 
                            VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
 
                         """
@@ -36,6 +56,107 @@ class IqiyicrawlerPipeline(object):
                 item['sex'],
                 item['ages'],
                 item['duration']
+            )
+            # print(sql)
+            try:
+                self.ora.cux_sql_base(self.ora.connect(), sql)
+            except Exception as e:
+                print(str(e) + "  " + item['name'])
+
+        if isinstance(item, IqiyiTVItem):
+            sql = self.Tv_Sql % (
+                item['url'],
+                item['name'],
+                item['directors'],
+                item['Actors'],
+                item['period'],
+                item['description'],
+                item['pay'],
+                item['score'],
+                item['starTotal'],
+                item['tags'],
+                item['type'],
+                item['region'],
+                item['positive'],
+                item['sex'],
+                item['ages'],
+                item['subtitle'],
+                item['numTotal']
+            )
+            # print(sql)
+            try:
+                self.ora.cux_sql_base(self.ora.connect(), sql)
+            except Exception as e:
+                print(str(e) + "  " + item['name'])
+
+        if isinstance(item, IqiyiCartoonItem):
+            sql = self.cartoon_Sql % (
+                item['url'],
+                item['name'],
+                item['Actors'],
+                item['period'],
+                item['description'],
+                item['pay'],
+                item['score'],
+                item['total'],
+                item['type'],
+                item['region'],
+                item['sex'],
+                item['ages'],
+                item['subtitle'],
+                item['number'],
+                item['version'],
+                item['inLanguage']
+            )
+            # print(sql)
+            try:
+                self.ora.cux_sql_base(self.ora.connect(), sql)
+            except Exception as e:
+                print(str(e) + "  " + item['name'])
+
+        if isinstance(item, IqiyiChildItem):
+            sql = self.child_Sql % (
+                item['url'],
+                item['name'],
+                item['Actors'],
+                item['period'],
+                item['description'],
+                item['pay'],
+                item['score'],
+                item['total'],
+                item['type'],
+                item['region'],
+                item['sex'],
+                item['ages'],
+                item['subtitle'],
+                item['number'],
+                item['version'],
+                item['inLanguage']
+            )
+            # print(sql)
+            try:
+                self.ora.cux_sql_base(self.ora.connect(), sql)
+            except Exception as e:
+                print(str(e) + "  " + item['name'])
+
+        if isinstance(item, IqiyiShowItem):
+            sql = self.show_Sql % (
+                item['url'],
+                item['name'],
+                item['Actors'],
+                item['period'],
+                item['description'],
+                item['pay'],
+                item['score'],
+                item['total'],
+                item['type'],
+                item['region'],
+                item['sex'],
+                item['ages'],
+                item['subtitle'],
+                item['number'],
+                item['version'],
+                item['inLanguage']
             )
             # print(sql)
             try:
